ea3fb1719400f6e76ba971a18d81790636a36a30
alcrownll <goddessphoebevaldehuesa@gmail.com>
2025-05-01 16:04:05 +0800

    first commit

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfVxyOWhp4JkS8d4V0P0DbN2mzUEvwoXU",
+  authDomain: "vibe-compiler.firebaseapp.com",
+  projectId: "vibe-compiler",
+  storageBucket: "vibe-compiler.appspot.com",
+  messagingSenderId: "27745695117",
+  appId: "1:27745695117:web:bb896ca441b7aa291879c4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: [
+      './index.html',
+      './src/**/*.{js,jsx,ts,tsx}', // Adjust paths to your source files as needed
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
